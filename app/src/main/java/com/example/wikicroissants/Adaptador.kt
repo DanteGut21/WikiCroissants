26fb700dcfa9b8cab6290ce814b17ee176163b54
Dante Gutierrez <2121200441@soy.utj.edu.mx>
2024-03-18 01:42:36 -0600

    v1.1.2

--- app/src/main/java/com/example/wikicroissants/Adaptador.kt
+++ app/src/main/java/com/example/wikicroissants/Adaptador.kt
@@ -3,29 +3,55 @@ package com.example.wikicroissants
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class Adaptador (private val dataList: List<String>) : RecyclerView.Adapter<Adaptador.MyViewHolder>(){
+class Adaptador (private val dataList: List<String>) :
+    RecyclerView.Adapter<Adaptador.ElementoViewHolder>(){
+//
+//    // Proporciona una referencia a las vistas para cada elemento de datos
+//    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+//        val textView: TextView = itemView.findViewById(R.id.tvEstante)
+//    }
+//
+//    // Crea nuevas vistas (invocadas por el layout manager)
+//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+//        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+//        return MyViewHolder(itemView)
+//    }
+//
+//    // Reemplaza el contenido de una vista (invocada por el layout manager)
+//    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+//        val currentItem = dataList[position]
+//        holder.textView.text = currentItem
+//    }
+//
+//    // Retorna el tamaño de tu dataset (invocado por el layout manager)
+//    override fun getItemCount() = dataList.size
 
-    // Proporciona una referencia a las vistas para cada elemento de datos
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val textView: TextView = itemView.findViewById(R.id.tvEstante)
+    // Define una clase para el ViewHolder
+    class ElementoViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView = view.findViewById(R.id.tvEstante)
+        val imageView: ImageView = view.findViewById(R.id.imgEstante)
     }
 
-    // Crea nuevas vistas (invocadas por el layout manager)
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
-        return MyViewHolder(itemView)
+    // Crea nuevas vistas
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ElementoViewHolder {
+        // Crea una nueva vista usando el layout definido anteriormente
+        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.item_layout, parent, false)
+        return ElementoViewHolder(adapterLayout)
     }
 
-    // Reemplaza el contenido de una vista (invocada por el layout manager)
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val currentItem = dataList[position]
-        holder.textView.text = currentItem
+    // Reemplaza el contenido de una vista
+    override fun onBindViewHolder(holder: ElementoViewHolder, position: Int) {
+        val item = dataList[position]
+        holder.textView.text = item
+        // Establece la misma imagen predefinida para todos los elementos
+        holder.imageView.setImageResource(R.drawable.libro)
     }
 
-    // Retorna el tamaño de tu dataset (invocado por el layout manager)
+    // Devuelve el tamaño de tu conjunto de datos
     override fun getItemCount() = dataList.size
 
-}
+}//Class Adaptadror
